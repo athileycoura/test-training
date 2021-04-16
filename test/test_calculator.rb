2a7e38a2662bd1541cb60869fe25808b1fc1446f
Athiley Luciano <athileyluciano@gmail.com>
2021-04-16 13:58:47 +0200

    adding test to check sum operation when numbers have same absolute value and different signals and adding test to check sum operation with both numbers are positive

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -7,16 +7,39 @@ class TestCalculator < Test::Unit::TestCase
     first_number = -1
     second_number = -1
     calculator = Calculator.new
-    #then
-    actual_result = calculator.sum(first_number, second_number)
     #when
+    actual_result = calculator.sum(first_number, second_number)
+    #then
     expected_result = -2
+    assert_equal(expected_result, actual_result)
+  end
+
+  def test_should_return_zero_when_adding_negative_two_with_positive_two
+    #given
+    first_number = -2
+    second_number = 2
+    calculator = Calculator.new
+    #when
+    actual_result = calculator.sum(first_number, second_number)
+    #then
+    expected_result = 0
+    assert_equal(expected_result, actual_result)
+  end
+
+  def test_should_return_ten_when_adding_positive_five_and_positive_five
+    #given
+    first_number = 5
+    second_number = 5
+    calculator = Calculator.new
+    #when
+    actual_result = calculator.sum(first_number, second_number)
+    #then
+    expected_result = 10
     assert_equal(expected_result, actual_result)
   end
 end
 
-# deve retornar um número negativo quando somando dois números negativos
-# deve retornar zero quando somado um número de mesmo valor sendo um negativo e outro positivo
 # deve retornar positivo quando somado dois numeros positivos
 # deve retornar uma exceção com mensagem de erro quando um dos valores não é um inteiro
 # deve retornar o sinal do maior numero quando somado um negativo com um positivo
+# a ordem dos fatores nao altera o produto
